4d17b2efb155abc54996980000b8cf8b5c8ba832
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-31 19:40:01 +0100

    frontend in

--- backend/routers/skills.py
+++ backend/routers/skills.py
@@ -1,5 +1,5 @@
 import json
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 router = APIRouter(
 	prefix="/skills"
@@ -10,3 +10,13 @@ async def get_skills():
 	with open("data/skills.json", "r") as file:
 		skills = json.load(file)
 	return skills
+
+@router.get("/{id}")
+async def get_skill(id:int):
+	with open("data/skills.json", "r") as file:
+		skills = json.load(file)
+	try: 
+		skill = next(skill for skill in skills if skill["id"] == id)
+		return skill
+	except:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="skill not found")
