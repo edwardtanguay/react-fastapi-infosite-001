c1863f500854333e440eb4dcf59547901031f6f0
Edward Tanguay (windows) <edward@tanguay.info>
2025-01-01 20:03:13 +0100

    showing skills

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,9 +1,31 @@
-import { useTypedStoreState } from "../store/hooks"
+import axios from "axios";
+import { useTypedStoreState } from "../store/hooks";
+import { useEffect, useState } from "react";
 
 export const PageWelcome = () => {
 	const { message } = useTypedStoreState((state) => state.mainModel);
+	const [skills, setSkills] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get("http://localhost:3355/skills");
+			const _skills = response.data;
+			setSkills(_skills);
+		})();
+	}, []);
 
 	return (
-		<p>{message}</p>
-	)
-}
+		<>
+			<p>{message}</p>
+
+			<h2 className="mt-3 text-xl">{skills.length} Skills</h2>
+			<ul>
+				{skills.map(skill => {
+					return (
+						<li key={skill.id}>{skill.name}</li>
+					)
+				})}
+			</ul>
+		</>
+	);
+};
